7c930ae4aa5ef0a856d25a9299aff1d116870712
S2000239\Snetsystems <lee.chanho@snetsystems.co.kr>
2023-09-21 11:32:38 +0900

    [test] 배포 테스트 (1)

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,21 @@ router.get('/', function(req, res, next) {
   res.render('index.html');
 });
 
+router.post("/sayHello", function (req, res) {
+  console.log(req.body);
+  const responseBody = {
+    version: "2.0",
+    template: {
+      outputs: [
+        {
+          simpleText: {
+            text: "hello I'm Ryan",
+            info: "내쏘서뤼스"
+          },
+        },
+      ],
+    },
+  };
+  res.status(200).send(responseBody);
+});
 module.exports = router;
