a603c6bbc61fcb606e960c1a5ec044bddf248134
S2000239\Snetsystems <lee.chanho@snetsystems.co.kr>
2023-09-21 11:19:56 +0900

    [fix] pakage.json 수정

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const nunjucks = require('nunjucks');
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
