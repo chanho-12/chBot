2ede26e84a87d2500b59988e1b5959590bb7d399
S2000239\Snetsystems <lee.chanho@snetsystems.co.kr>
2023-09-26 11:36:52 +0900

    [fix0] fix

--- routes/index.js
+++ routes/index.js
@@ -79,91 +79,92 @@ const getInfo = async (req, res, char) => {
     // 3
     // const bodyList = $("div.absolute top-0 -left-0.5");
     const bodyList = $("div.flex.flex-col.w-full");
+    console.log("!!!!!", bodyList.find("text-sm text-positive-lesser"));
+      bodyList.map((i, element) => {
+        ulList[i] = {
+          server: $(element)
+            .find("p.space-x-2.text-xs span.inline-block:nth-child(1)")
+            .text(),
+          job: $(element)
+            .find("p.space-x-2.text-xs span.inline-block:nth-child(2)")
+            .text(),
+          name: $(element).find("p.text-2xl").text(),
+          chingho: $(element).find("p.text-sm.opacity-70").text(),
+          level: $(element)
+            .find(
+              "div.absolute.flex.space-x-6.bottom-7 div:nth-child(1) div:nth-child(2).text-xl"
+            )
+            .text(),
+          warLevel: $(element)
+            .find(
+              "div.absolute.flex.space-x-6.bottom-7 div:nth-child(2) div:nth-child(2).text-xl"
+            )
+            .text(),
+          travelLevel: $(element)
+            .find(
+              "div.absolute.flex.space-x-6.bottom-7 div:nth-child(3) div:nth-child(2).text-xl"
+            )
+            .text(),
+          guild: $(element)
+            .find("div.absolute.right-0.top-14 div span:nth-child(1)")
+            .text(),
+          og: $(element)
+            .find("div.absolute.right-0.top-14 div span:nth-child(3)")
+            .text(),
+          pvp: $(element)
+            .find("div.absolute.right-0.top-14 div span:nth-child(5)")
+            .text(),
+          th: $(element)
+            .find(
+              "div.flex-1.flex.gap-4.justify-between div:nth-child(1) p.text-lg"
+            )
+            .text(),
+          cm: $(element)
+            .find(
+              "div.flex-1.flex.gap-4.justify-between div:nth-child(2) p.text-lg"
+            )
+            .text(),
+          ss: $(element)
+            .find(
+              "div.flex-1.flex.gap-4.justify-between div:nth-child(3) p.text-lg"
+            )
+            .text(),
+          jobType: $(element)
+            .find(
+              "div.flex.flex-col.bg-negative.py-2 div:nth-child(1) p.text-positive-less.text-sm.leading-none.w-full"
+            )
+            .text(),
+          elixir: $(element)
+            .find(
+              "div.cursor-pointer:nth-child(2) div.flex.items-center.gap-4 div.space-y-1 p:nth-child(1)"
+            )
+            .text(),
+          elixir2: $(element)
+            .find(
+              "div.cursor-pointer:nth-child(2) div.flex.items-center.gap-4 div.space-y-1 p:nth-child(2)"
+            )
+            .text(),
+          gemstone: $(element)
+            .find(
+              "div.flex.flex-col.flex-1.gap-4 div:nth-child(2) div.flex.justify-between.cursor-pointer.select-none div.flex.items-center.gap-2 span:nth-child(3)"
+            )
+            .text(),
+          card: $(element)
+            .find(
+              "div.flex.flex-col.flex-1.gap-4 div:nth-child(3) div.flex.justify-between.cursor-pointer.select-none div.flex.items-center.gap-2 span:nth-child(1)"
+            )
+            .text(),
+        };
+        console.log("type", typeof ulList[0]);
+        console.log("bodyList : ", ulList[0]);
+      });
     
-    bodyList.map((i, element) => {
-      ulList[i] = {
-        server: $(element)
-          .find("p.space-x-2.text-xs span.inline-block:nth-child(1)")
-          .text(),
-        job: $(element)
-          .find("p.space-x-2.text-xs span.inline-block:nth-child(2)")
-          .text(),
-        name: $(element).find("p.text-2xl").text(),
-        chingho: $(element).find("p.text-sm.opacity-70").text(),
-        level: $(element)
-          .find(
-            "div.absolute.flex.space-x-6.bottom-7 div:nth-child(1) div:nth-child(2).text-xl"
-          )
-          .text(),
-        warLevel: $(element)
-          .find(
-            "div.absolute.flex.space-x-6.bottom-7 div:nth-child(2) div:nth-child(2).text-xl"
-          )
-          .text(),
-        travelLevel: $(element)
-          .find(
-            "div.absolute.flex.space-x-6.bottom-7 div:nth-child(3) div:nth-child(2).text-xl"
-          )
-          .text(),
-        guild: $(element)
-          .find("div.absolute.right-0.top-14 div span:nth-child(1)")
-          .text(),
-        og: $(element)
-          .find("div.absolute.right-0.top-14 div span:nth-child(3)")
-          .text(),
-        pvp: $(element)
-          .find("div.absolute.right-0.top-14 div span:nth-child(5)")
-          .text(),
-        th: $(element)
-          .find(
-            "div.flex-1.flex.gap-4.justify-between div:nth-child(1) p.text-lg"
-          )
-          .text(),
-        cm: $(element)
-          .find(
-            "div.flex-1.flex.gap-4.justify-between div:nth-child(2) p.text-lg"
-          )
-          .text(),
-        ss: $(element)
-          .find(
-            "div.flex-1.flex.gap-4.justify-between div:nth-child(3) p.text-lg"
-          )
-          .text(),
-        jobType: $(element)
-          .find(
-            "div.flex.flex-col.bg-negative.py-2 div:nth-child(1) p.text-positive-less.text-sm.leading-none.w-full"
-          )
-          .text(),
-        elixir: $(element)
-          .find(
-            "div.cursor-pointer:nth-child(2) div.flex.items-center.gap-4 div.space-y-1 p:nth-child(1)"
-          )
-          .text(),
-        elixir2: $(element)
-          .find(
-            "div.cursor-pointer:nth-child(2) div.flex.items-center.gap-4 div.space-y-1 p:nth-child(2)"
-          )
-          .text(),
-        gemstone: $(element)
-          .find(
-            "div.flex.flex-col.flex-1.gap-4 div:nth-child(2) div.flex.justify-between.cursor-pointer.select-none div.flex.items-center.gap-2 span:nth-child(3)"
-          )
-          .text(),
-        card: $(element)
-          .find(
-            "div.flex.flex-col.flex-1.gap-4 div:nth-child(3) div.flex.justify-between.cursor-pointer.select-none div.flex.items-center.gap-2 span:nth-child(1)"
-          )
-          .text(),
-      };
-      console.log("type", typeof(ulList[0]));
-      console.log("bodyList : ", ulList[0]);
-    });
     // return getList.push(ulList[0]);
     return res.status(200).send(ulList[0]);
     
     // return ulList[0];
   } catch (error) {
-    console.error(error);
+    console.error("error" ,error);
     return res.status(200).send({name: undefined});
   }
 };
